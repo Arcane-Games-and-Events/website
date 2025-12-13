f5c49c1bdea360a315ef9eae144136a027257dc9
Justin Liwag <justinlwg@gmail.com>
2025-12-13 01:46:49 -0800

    Exempt webhook endpoints from CSRF protection
    Authorize.net Silent Post webhooks don't send an Origin header,
    which triggers SvelteKit's CSRF protection. This exempts all
    /api/webhooks/* paths from the check.

--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,18 @@ const config = {
 	kit: {
 		adapter: adapter({
 			runtime: 'nodejs22.x'
-		})
+		}),
+		csrf: {
+			// Allow webhooks from Authorize.net (they don't send Origin header)
+			checkOrigin: (event) => {
+				const path = event.url.pathname;
+				// Exempt webhook endpoints from CSRF protection
+				if (path.startsWith('/api/webhooks/')) {
+					return false;
+				}
+				return true;
+			}
+		}
 	}
 };
 
